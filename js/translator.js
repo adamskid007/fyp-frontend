9887f34d1466cc7232cbf32f2ff2306f16818712
adamskid007 <83013775+adamskid007@users.noreply.github.com>
2023-07-05 14:26:34 +0100

    Add files via upload

---
+++ js/translator.js
@@ -0,0 +1,40 @@
+async function translate(input) {
+    const response = await fetch("/translate", {
+        method: "POST",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify({
+            input
+        })
+    })
+    
+    if (!response.ok) {
+        throw new Error(await response.text())
+    }
+
+    return response.text()
+}
+
+(async function() {
+    const inputFr = document.getElementById("input-text-fr")
+    const outputIg = document.getElementById("output-ig")
+    const btnTranlate = document.getElementById("btn-translate")
+
+    btnTranlate.addEventListener("click", async () => {
+        const textFr = inputFr.value.trim()
+
+        if (textFr.length == 0) {
+            alert("Enter a text in french")
+            return
+        }
+
+        try {
+            const textIg = await translate(textFr)
+            outputIg.value = textIg
+        } catch(e) {
+            alert(e.message)
+        }
+
+    })
+})()
